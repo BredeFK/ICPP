9c3992d25cb031a55d2b8e732cf29ebfc98454e8
bredefk <bredefk@stud.ntnu.no>
2019-05-20 15:57:52 +0200

    Can now get active/inactive/all users

--- classes/Users.php
+++ classes/Users.php
@@ -8,17 +8,33 @@ class Users
     // Returns a number of how many users in db
     public static function getTotalUsers($db)
     {
-        $sql = "SELECT COUNT(id) FROM users";
+        $sql = "SELECT users.id, users.name, users.email_student, users.teacher, users.email_private FROM users";
         $stmt = $db->prepare($sql);
         $stmt->execute();
 
-        $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-        $element = reset($users);
-        if($element == false){
-            return 0;
-        } else {
-            return $element;
-        }
+    // Returns a number of how many users that are not in a cours in db
+    public static function getUsersInactive($db)
+    {
+        $sql = "SELECT users.id, users.name, users.email_student, users.teacher, users.email_private FROM users
+        LEFT JOIN usercourse ON usercourse.userid = users.id
+        WHERE usercourse.id IS NULL";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Returns a number of how many users that are in a cours in db
+    public static function getUsersActive($db)
+    {
+        $sql = "SELECT DISTINCT users.id, users.name, users.email_student, users.teacher, users.email_private FROM users
+        INNER JOIN usercourse ON usercourse.userid = users.id";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- php/test.php
+++ php/test.php
@@ -9,7 +9,33 @@ require_once "$ROOT/classes/Users.php";
 
 $db = ICPP::requireDatabase();
 
-$number = Users::getTotalUsers($db);
+// Get users data
+$users = Users::getTotalUsers($db);
+$inactiveUsers = Users::getUsersInactive($db);
+$activeUsers = Users::getUsersActive($db);
 
-echo "Number of students is: " . Users::getTotalUsers($db) . '<br>';
-echo "type is " . gettype($number);
+// Display all users
+echo "<h1>Users</h1>";
+echo "<h2>getTotalUsers</h2>";
+echo "Variable type is: " . gettype($users). '<br>';
+echo "Count is: " . count($users) . '<br><br>';
+foreach($users as $result) {
+    echo $result['id'] . " " . $result['name'] . " " . $result['email_student'] . " " . $result['teacher'] . " " . $result['email_private'], '<br>';
+}
+
+// Display inactive users
+echo "<h2>getUsersInactive</h2>";
+echo "Variable type is: " . gettype($inactiveUsers). '<br>';
+echo "Count is: " . count($inactiveUsers) . '<br><br>';;
+foreach($inactiveUsers as $result) {
+    echo $result['id'] . " " . $result['name'] . " " . $result['email_student'] . " " . $result['teacher'] . " " . $result['email_private'], '<br>';
+}
+
+// Display active users
+echo "<h2>getUsersActive</h2>";
+echo "Variable type is: " . gettype($activeUsers). '<br>';
+echo "Count is: " . count($activeUsers) . '<br><br>';;
+foreach($activeUsers as $result) {
+    echo $result['id'] . " " . $result['name'] . " " . $result['email_student'] . " " . $result['teacher'] . " " . $result['email_private'], '<br>';
+}
+
