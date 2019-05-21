c22674921274c49f74c336f18d55134c6dd51707
BredeFK <bredefk@stud.ntnu.no>
2019-05-21 17:50:58 +0200

    minor fix

--- classes/UserReviews.php
+++ classes/UserReviews.php
@@ -3,8 +3,23 @@
 // Class for the user_reviews table
 class UserReviews{
 
+    /*
     // Returns all reviews for all assignments
     public static function getAllReviewsForAllAssignments($db) {
+        $sql = "SELECT COUNT(DISTINCT user_reviews.submitted) as reviews, assignments.name AS assignment
+        FROM user_reviews
+        INNER JOIN fields ON user_reviews.name = fields.name 
+        INNER JOIN assignments ON user_reviews.assignment_id = assignments.id
+        GROUP BY user_reviews.assignment_id";  
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    */
+
+    // Returns the top 15 users that has reviewd submissions the most
+    public static function getTopReviewers($db) {
         $sql = "SELECT COUNT(DISTINCT user_reviews.submitted) as reviews, assignments.name AS assignment
         FROM user_reviews
         INNER JOIN fields ON user_reviews.name = fields.name 
